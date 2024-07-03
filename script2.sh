c6c6c4f7bfa2f73ae1e4a6477acc30d38f9bd7ce
lexx <lexx@mail.de>
2024-07-03 14:49:46 +0000

    Update

--- script2.sh
+++ script2.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+summa = 0
+
+myFunction()
+{
+	echo "Functions param is $1"
+	summa=$1
+}
+
 if [ "$0" == "./script1.sh" ]; then
 	echo "Script 1"
 elif [ "$0" == "./script2.sh" ]; then
@@ -16,4 +24,35 @@ case $x in
         *) echo "Unknown"
 esac
 
+#echo "yes/no"
+#read x
 
+#read -p "yes/no :" x
+#echo "$x"
+
+counter=0
+
+while [ $counter -lt 10 ]; do
+	echo "counter is $counter"
+	#let counter=+1
+	counter=$((counter+1))
+done
+
+for file in `ls *.sh`; do
+	echo "$file"
+done
+
+
+for x in {1..10}; do
+	echo "$x"
+done
+
+
+for (( i=1; i<=10; i++)); do
+	echo "$i"
+done
+
+
+echo "summa is $summa"
+myFunction 100
+echo "summa is $summa"
