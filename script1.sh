c23c46144f42b760d003ac2d59001289c4b1d25c
EC2 Default User <ec2-user@ip-172-31-45-10.ec2.internal>
2024-07-03 13:35:28 +0000

    Update

--- script1.sh
+++ script1.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-xterm &
+
+myOS=`uname -a`
+
+echo "My name is $0"
+
+num1=10
+num2=20
+summa=$((num1+num2))
+
+echo "Summa is $summa"
+
+echo "My OS is $myOS"
+
+myhost=`hostname`
+mygtw="8.8.8.8"
+
+ping -c 4 $myhost
+ping -c 4 $mygtw
+
+
+
 
